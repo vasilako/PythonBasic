0952d9e5394f30b1f7566861c63445180defd52b
vasilako <vasilako@hotmail.com>
2021-04-27 13:21:35 +0200

    Create car fuel consumption_BG.py

---
+++ car fuel consumption_BG.py
@@ -0,0 +1,41 @@
+
+#------------------Autor: Всасил--------------------------------------
+#-----------------Fecha  20210427---version v1- ----------------------------------
+
+
+#--------------Declaración de funciones --------------------------------
+def calculoConsomo(litros, kilomertosTrascutidos):
+    consumo = litros/kilomertosTrascutidos*100
+    return consumo
+
+def costePorKilometro(consumo, precioCombustible):
+    costeKM = consumo*precioCombustible/100
+    return costeKM
+    
+
+#---------------------Inputs de los datos-------------------------------
+litros = float(input("Колко литра си раредил?: "))
+kilomertosTrascutidos = int(input("Колко километра си изминал: "))
+precioCombustible = float(input("Цента нагоривото за литър_: "))
+elegirCaclculo = int(input("Натисни едно за да изчислиш разхода за 100 Км /n, натисни 2 за да изчислиш стойноста на километър"))
+
+
+#------------------Condicional eleccion de la funcion--------------------
+
+if elegirCaclculo == 1:
+        print("Разхода е: ", calculoConsomo(litros, kilomertosTrascutidos), "на 100 Km")
+
+
+#----------v.1-1------LLAMADA A LA PRIMERA FUNCION costePorKilometro(calculoConsomo(litros, kilomertosTrascutidos), precioCombustible)--------------    
+elif elegirCaclculo == 2:
+     print("Стойноста на иминат километър е: ", costePorKilometro(calculoConsomo(litros, kilomertosTrascutidos), precioCombustible), "Лв")
+
+#------------------------------v.1-1-1------------------------
+else:
+    print("Това число не е валидно натисни 1 или 2 ")
+    while elegirCaclculo != 1 or 2:
+            elegirCaclculo = int(input("Натисни едно за да изчислиш разхода за 100 Км /n, натисни 2 за да изчислиш стойноста на километър"))
+
+
+
+      
