0aac21327ebbcb843356011c3efddf02e24f3674
vasilako <vasilako@hotmail.com>
2021-04-27 13:28:56 +0200

    Update car fuel consumption_BG.py

--- car fuel consumption_BG.py
+++ car fuel consumption_BG.py
@@ -16,25 +16,25 @@ def costePorKilometro(consumo, precioCombustible):
 #---------------------Inputs de los datos-------------------------------
 litros = float(input("Колко литра си раредил?: "))
 kilomertosTrascutidos = int(input("Колко километра си изминал: "))
-precioCombustible = float(input("Цента нагоривото за литър_: "))
-elegirCaclculo = int(input("Натисни едно за да изчислиш разхода за 100 Км /n, натисни 2 за да изчислиш стойноста на километър"))
+precioCombustible = float(input("Каква е цента нагоривото за литър: "))
+elegirCaclculo = int(input("Натисни едно за да изчислиш разхода за 100 Км, натисни 2 за да изчислиш стойността на километър: "))
 
 
 #------------------Condicional eleccion de la funcion--------------------
 
 if elegirCaclculo == 1:
-        print("Разхода е: ", calculoConsomo(litros, kilomertosTrascutidos), "на 100 Km")
+        print("Разхода на гориво е: ", calculoConsomo(litros, kilomertosTrascutidos), "на 100 Km")
 
 
 #----------v.1-1------LLAMADA A LA PRIMERA FUNCION costePorKilometro(calculoConsomo(litros, kilomertosTrascutidos), precioCombustible)--------------    
 elif elegirCaclculo == 2:
-     print("Стойноста на иминат километър е: ", costePorKilometro(calculoConsomo(litros, kilomertosTrascutidos), precioCombustible), "Лв")
+     print("Стойността на иминат километър е: ", costePorKilometro(calculoConsomo(litros, kilomertosTrascutidos), precioCombustible), "Лв")
 
 #------------------------------v.1-1-1------------------------
 else:
     print("Това число не е валидно натисни 1 или 2 ")
     while elegirCaclculo != 1 or 2:
-            elegirCaclculo = int(input("Натисни едно за да изчислиш разхода за 100 Км /n, натисни 2 за да изчислиш стойноста на километър"))
+            elegirCaclculo = int(input("Натисни едно за да изчислиш разхода за 100 Км, натисни 2 за да изчислиш стойността на километър: "))
 
 
 
